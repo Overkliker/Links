fd9c3f8a025c3e176e7c833659d55febac2119f9
kliker <danilahreno@gmail.com>
2023-02-28 22:05:38 +0300

    Добавьте файлы проекта.

---
+++ Links/Links.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+
+int fac(int ch) {
+    int& a = ch;
+    if (a == 1) {
+        return 1;
+    }
+    return a * fac(a - 1);
+}   
+
+void input() {
+    setlocale(LC_ALL, "Russian");
+    int ch;
+    cout << "Введите переменную: ";
+    cin >> ch;
+    
+    cout << fac(ch);
+    cout << "\n";
+}
+
+int main()
+{
+    input();
+    return 0;
+}
